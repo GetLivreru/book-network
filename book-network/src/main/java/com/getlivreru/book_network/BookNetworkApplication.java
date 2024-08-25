85007ef6393c24a95ea4d16e95512ec0044a0c50
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-25 23:55:17 +0500

    Initial commit

---
+++ book-network/src/main/java/com/getlivreru/book_network/BookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.getlivreru.book_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApplication.class, args);
+	}
+
+}
---
+++ book-network/src/test/java/com/getlivreru/book_network/BookNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getlivreru.book_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
