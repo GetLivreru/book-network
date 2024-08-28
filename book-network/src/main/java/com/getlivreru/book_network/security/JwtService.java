6a0ed49d06a21fcbf83e3fe1ed623b43398baf28
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-29 01:03:49 +0500

    add jwt token auth

---
+++ book-network/src/main/java/com/getlivreru/book_network/security/JwtService.java
@@ -0,0 +1,4 @@
+package com.getlivreru.book_network.security;
+
+public class JwtService {
+}
--- book-network/src/main/java/com/getlivreru/book_network/security/SecurityConfig.java
+++ book-network/src/main/java/com/getlivreru/book_network/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.getlivreru.book_network.security;
 
+import com.getlivreru.book_network.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
