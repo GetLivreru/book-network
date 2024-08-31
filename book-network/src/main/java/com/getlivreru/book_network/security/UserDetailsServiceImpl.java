5f0ca0c0d7a503f3b502879b6b04678cdc3a3ee4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-31 23:00:55 +0500

    add validation for UserDetailsServiceImpl

---
+++ book-network/src/main/java/com/getlivreru/book_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.getlivreru.book_network.security;
+
+import com.getlivreru.book_network.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+        private final UserRepository userRepository;
+        @Override
+        @Transactional
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            return userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        }
+    }
+
