ee0aae67c24bae72f41e1ca70a03abe6583c6b83
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-01 00:33:31 +0500

    filling code for EmailService

--- book-network/src/main/java/com/getlivreru/book_network/email/EmailTemplateName.java
+++ book-network/src/main/java/com/getlivreru/book_network/email/EmailTemplateName.java
@@ -1,4 +1,16 @@
 package com.getlivreru.book_network.email;
 
-public @interface EmailTemplateName {
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account")
+    ;
+
+
+    private final String name;
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
 }
