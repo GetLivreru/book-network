d3cac271edc41718a043d7e258b9970cebf29a1e
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-31 22:26:12 +0500

    debugging jwtservice

--- book-network/src/main/java/com/getlivreru/book_network/security/JwtService.java
+++ book-network/src/main/java/com/getlivreru/book_network/security/JwtService.java
@@ -79,7 +79,7 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
+                .parser()
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
