952f97625f1ca8b0882872458c6468edbf2eb921
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-01 00:12:48 +0500

    filling authreq and authres

--- book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationResponse.java
+++ book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationResponse.java
@@ -1,4 +1,13 @@
 package com.getlivreru.book_network.auth;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class AuthenticationResponse {
+    private String token;
 }
