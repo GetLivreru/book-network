7dd01f64d67be9949d6fa2049dcf53fbfc4953f4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-01 00:02:12 +0500

    creating AuthenticationService

--- book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationController.java
+++ book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationController.java
@@ -2,12 +2,20 @@ package com.getlivreru.book_network.auth;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
-
+@Tag(name = "Authentication")
 public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
 }
--- book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationService.java
+++ book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationService.java
@@ -1,4 +1,20 @@
 package com.getlivreru.book_network.auth;
 
+import com.getlivreru.book_network.role.RoleRepository;
+import com.getlivreru.book_network.security.JwtService;
+import com.getlivreru.book_network.user.TokenRepository;
+import com.getlivreru.book_network.user.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final RoleRepository roleRepository;
+    private final EmailService emailService;
+    private final TokenRepository tokenRepository;
+
 }
