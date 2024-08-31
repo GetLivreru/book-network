ce25787a18bb028e76cbdf5d405f9f18df368ceb
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-31 23:27:02 +0500

    creating first controller

---
+++ book-network/src/main/java/com/getlivreru/book_network/auth/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.getlivreru.book_network.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+
+public class AuthenticationController {
+}
