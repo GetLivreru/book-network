ca4123d589808ca2d71542e7635b6bd03ca5d1c4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-06 22:35:58 +0500

    creating two exception classes

---
+++ book-network/src/main/java/com/getlivreru/book_network/expection/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.getlivreru.book_network.expection;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
---
+++ book-network/src/main/java/com/getlivreru/book_network/expection/OperationNotPermittedException.java
@@ -0,0 +1,11 @@
+package com.getlivreru.book_network.expection;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException() {
+    }
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+}
