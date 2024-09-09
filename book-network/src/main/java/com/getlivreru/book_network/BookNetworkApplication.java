04f08421c4606d7a54c9b17c16d0285658e2578f
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-09 17:58:54 +0500

    create openapi for ui

--- book-network/src/main/java/com/getlivreru/book_network/BookNetworkApplication.java
+++ book-network/src/main/java/com/getlivreru/book_network/BookNetworkApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
