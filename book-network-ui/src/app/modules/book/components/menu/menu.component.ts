9028558aa51039baba83b68bdf874ccb6c7c02d4
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-10 17:54:29 +0500

    editing menu part

--- book-network-ui/src/app/modules/book/components/menu/menu.component.ts
+++ book-network-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from '../../../../services/keycloak/keycloak.service';
 
 @Component({
   selector: 'app-menu',
-  standalone: true,
-  imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
+  constructor(
+    private keycloakService: KeycloakService
+  ) {
+  }
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(link => {
+      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+      });
+    });
+  }
+
+  async logout() {
+    await this.keycloakService.logout();
+  }
 }
