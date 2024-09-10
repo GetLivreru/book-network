414b88bfd270b39d4217ec9bb639210ebf16985c
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-10 17:52:45 +0500

    ng g c create-something

---
+++ book-network-ui/src/app/modules/book/components/rating/rating.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  standalone: true,
+  imports: [],
+  templateUrl: './rating.component.html',
+  styleUrl: './rating.component.scss'
+})
+export class RatingComponent {
+
+}
