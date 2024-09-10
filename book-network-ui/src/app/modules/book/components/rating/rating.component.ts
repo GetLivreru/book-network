c1bbb726a6a334fcf892e124665ef5321e129631
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-10 17:55:26 +0500

    rating.component

--- book-network-ui/src/app/modules/book/components/rating/rating.component.ts
+++ book-network-ui/src/app/modules/book/components/rating/rating.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-rating',
-  standalone: true,
-  imports: [],
   templateUrl: './rating.component.html',
-  styleUrl: './rating.component.scss'
+  styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent {
 
+  @Input() rating: number = 0;
+  @Output() ratingClicked: EventEmitter<number> = new EventEmitter<number>();
+  maxRating: number = 5;
+
+  get fullStars(): number {
+    return Math.floor(this.rating);
+  }
+
+  get hasHalfStar(): boolean {
+    return this.rating % 1 !== 0;
+  }
+
+  get emptyStars(): number {
+    return this.maxRating - Math.ceil(this.rating);
+  }
 }
