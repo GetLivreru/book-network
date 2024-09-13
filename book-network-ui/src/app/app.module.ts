7cbab12582f6a9abee52ef2c0158f8ba59806ec6
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-13 17:43:52 +0500

    that is a sh1t

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -3,25 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
-import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
-import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
-import {CodeInputModule} from 'angular-code-input';
-import {KeycloakService} from './services/keycloak/keycloak.service';
 
-export function kcFactory(kcService: KeycloakService) {
-  return () => kcService.init();
-}
+import {CodeInputModule} from 'angular-code-input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ActivateAccountComponent
+
   ],
   imports: [
     BrowserModule,
@@ -32,17 +24,7 @@ export function kcFactory(kcService: KeycloakService) {
   ],
   providers: [
     HttpClient,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpTokenInterceptor,
-      multi: true
-    },
-    {
-      provide: APP_INITIALIZER,
-      deps: [KeycloakService],
-      useFactory: kcFactory,
-      multi: true
-    }
+
 
   ],
   bootstrap: [AppComponent]
--- book-network-ui/src/app/services/keycloak/user-profile.ts
+++
@@ -1,7 +0,0 @@
-export interface UserProfile {
-  username?: string;
-  email?: string;
-  firstName?: string;
-  lastName?: string;
-  token?: string;
-}
