9cec8803f3120ad7f6f24c7cfdef137743172283
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-09-16 23:29:45 +0500

    create acctive account page

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 //import {authGuard} from './services/guard/auth.guard';
-//import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
+import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 
 const routes: Routes = [
   {
@@ -19,11 +19,11 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent
   },
-/*  {
+  {
     path: 'activate-account',
     component: ActivateAccountComponent
   },
-  {
+/*  {
     path: 'books',
     loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
     canActivate: [authGuard]
